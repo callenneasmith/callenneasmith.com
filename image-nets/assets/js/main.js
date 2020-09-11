0c46a44845677383e53f6c93ef2c4b8359357cda
Callen Neasmith <calneasmith@gmail.com>
2020-09-11 21:32:34 +1000

    image net build

--- image-nets/assets/js/main.js
+++ image-nets/assets/js/main.js
@@ -1,13 +1,43 @@
 $(document).ready(function(){
 	console.log(':)');
 
+	$('.btn-menu').click(function(){
+		$('.nav').addClass('slide')
+	});
+
+	$('.close').click(function(){
+		$('.nav').removeClass('slide')
+	});
 
 
 
+	$('.btn-reset').click(function(){
+		$('.item').removeClass('hide')
+	});
 
+	$('.btn-retail').click(function(){
+		$('.item').addClass('hide')
+		$('.item.retail').removeClass('hide')
+	});
 
+	$('.btn-les').click(function(){
+		$('.item').addClass('hide')
+		$('.item.les').removeClass('hide')
+	});
 
+	$('.btn-food').click(function(){
+		$('.item').addClass('hide')
+		$('.item.food').removeClass('hide')
+	});
 
+	$('.btn-ser').click(function(){
+		$('.item').addClass('hide')
+		$('.item.ser').removeClass('hide')
+	});
+	$('.btn-un').click(function(){
+		$('.item').addClass('hide')
+		$('.item.un').removeClass('hide')
+	});
 
 
 
