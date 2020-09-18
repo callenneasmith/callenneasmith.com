c698a4bf0277bd02dd4ac7570465cae225033c22
Callen Neasmith <calneasmith@gmail.com>
2020-09-18 10:44:05 +1000

    about me page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,13 @@
 $(document).ready(function(){
 
-var word1 = [
-	"Bob Hope",
-	"Pig's Ears",
-	"Rosy Lee",
-	"Tomfoolery",
-	"Vera Lynn",
-];
+$('.btn-about').click(function(){
+    $('.projects').addClass('hide');
+    $('.about').removeClass('hide');
+  });
 
-var randomWord1 = word1[Math.floor(Math.random()*word1.length)];
-
-var word2 = [
-  "Didn't Ought!",
-  "Gay and Hearty!",
-  "Light and Dark!",
-  "Near and Far!",
-  "Rattle and Clank!",
-];
-
-var randomWord2 = word2[Math.floor(Math.random()*word2.length)];
-
-var word3 = [
-  "China Plate.",
-  "Dustbin Lid.",
-  "Hampton Wick.",
-  "Old Bag.",
-  "Baked Bean.",
-];
-
-var randomWord3 = word3[Math.floor(Math.random()*word3.length)];
-
-$( ".word1" ).html(randomWord1);
-$( ".word2" ).html(randomWord2);
-$( ".word3" ).html(randomWord3);
+$('.btn-close').click(function(){
+    $('.projects').removeClass('hide');
+    $('.about').addClass('hide');
+  });
 
 });
