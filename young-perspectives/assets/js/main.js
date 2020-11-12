616ee7c01b687eac2bb7978027a495546829afde
Callen Neasmith <calneasmith@gmail.com>
2020-11-13 10:14:24 +1100

    scroller

--- young-perspectives/assets/js/main.js
+++ young-perspectives/assets/js/main.js
@@ -6,3 +6,16 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+$(document).ready(function(){
+	console.log(':)');
+
+	if ($(window).width() < 900) {
+		$('.footer').removeClass('b-top')
+	}
+
+	if ($(window).width() > 900) {
+		$('.footer').addClass('b-top')
+	}
+
+});
